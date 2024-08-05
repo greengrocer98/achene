ecdd7283fb371eeb2411c43e0cbc6e2342a86a44
greengrocer98 <esyunin.maks@mail.ru>
2024-08-05 11:24:05 +0300

    matrix and trackball pins changed

--- config.h
+++ config.h
@@ -6,11 +6,11 @@
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U
 
 /* SPI & PMW3360 settings. */
-#define SPI_DRIVER SPID1
-#define SPI_SCK_PIN GP10
-#define SPI_MOSI_PIN GP11
-#define SPI_MISO_PIN GP12
-#define PMW33XX_CS_PIN GP13
+// #define SPI_DRIVER SPID0
+#define PMW33XX_CS_PIN GP1
+#define SPI_SCK_PIN GP2
+#define SPI_MOSI_PIN GP3
+#define SPI_MISO_PIN GP4
 
 #define PMW33XX_CPI 600      // The CPI range is 100-12000, in increments of 100. Defaults to 1600 CPI.
 #define PMW33XX_CLOCK_SPEED 2000000 // Sets the clock speed that the sensor runs at. Defaults to 2000000
--- keymaps/vial/config.h
+++ keymaps/vial/config.h
@@ -2,5 +2,5 @@
 
 #define VIAL_KEYBOARD_UID {0x65, 0xD4, 0x05, 0x46, 0x4C, 0x62, 0x6C, 0x7E}
 
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_ROWS { 1, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 3, 4 }
