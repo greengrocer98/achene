04f1d5c6343a3f2ec4b7ac86cc50fe8023d6b5c1
ggrocer <esyunin.maks@mail.ru>
2024-08-08 13:12:02 +0300

    Changed diode direction

--- rules.mk
+++ rules.mk
@@ -1,7 +1,6 @@
 AUDIO_SUPPORTED = no          # Audio is not supported
 MOUSEKEY_ENABLE = yes         # Mouse keys
 EXTRAKEY_ENABLE = yes         # Audio control and System control
-POINTING_DEVICE_ENABLE = yes  # Trackball sensor
 CAPS_WORD_ENABLE = yes        # CapsWord support
 
 CONSOLE_ENABLE = no           # Console for debug
@@ -10,5 +9,6 @@ NKRO_ENABLE = no              # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no         # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no          # Enable keyboard RGB underglow
 
+POINTING_DEVICE_ENABLE = yes  # Trackball sensor
 POINTING_DEVICE_DRIVER = pmw3360
 SERIAL_DRIVER = vendor
