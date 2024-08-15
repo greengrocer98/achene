947781558d1f8aa55d9e862c5225721676702429
ggrocer <esyunin.maks@mail.ru>
2024-08-15 16:41:11 +0300

    small changes in keymap

--- keymaps/vial/keymap.c
+++ keymaps/vial/keymap.c
@@ -63,8 +63,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,
   // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
-              CTL_ENT, SFT_SPC, ALT_BSPC, RAISE_DEL,   GUI_GRAVE,                  LOWER_ESC
-  //      ╰────────────────────────────────────────╯ ╰───────────────────────────────────────╯
+            CTL_ENT, SFT_SPC, ALT_BSPC, RAISE_DEL,     GUI_GRAVE, LOWER_ESC
+  //      ╰────────────────────────────────────────╯ ╰──────────────────────╯
   ),
 
   [LAYER_LOWER] = LAYOUT(
@@ -73,10 +73,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, CW_TOGG,
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-      C(KC_BSLS), S(KC_LALT), KC_EQL, KC_MINS, KC_DOT,  KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_BSLS,
+      C(KC_BSLS), S(KC_EQL), KC_EQL, KC_MINS, KC_DOT,  KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_BSLS,
   // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
-              CTL_ENT, SFT_SPC, ALT_BSPC, XXXXXXX,      XXXXXXX,                  _______
-  //         ╰─────────────────────────────────────╯ ╰───────────────────────────────────╯
+            CTL_ENT, SFT_SPC, ALT_BSPC, S(KC_LALT),    XXXXXXX,     _______
+  //      ╰────────────────────────────────────────╯ ╰──────────────────────╯
   ),
 
   [LAYER_RAISE] = LAYOUT(
@@ -85,10 +85,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
          KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    ACCEL_0, KC_BTN1, KC_BTN2, KC_BTN3, XXXXXXX, 
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       KC_HOME, KC_PGUP, KC_PGDN,  KC_END, XXXXXXX,    ACCEL_1, CPI_UP, CPI_DOWN, XXXXXXX, XXXXXXX,
+        KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,    ACCEL_1, CPI_UP, CPI_DOWN, XXXXXXX, XXXXXXX,
   // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
-                XXXXXXX, _______, _______, XXXXXXX,    _______, XXXXXXX
-  //                   ╰───────────────────────────╯ ╰──────────────────╯
+                XXXXXXX, XXXXXXX, XXXXXXX, _______,    KC_LALT, KC_LSFT
+  //      ╰────────────────────────────────────────╯ ╰──────────────────────╯
   ),
 
   [LAYER_MOUSE] = LAYOUT(
@@ -99,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
        KC_HOME, KC_PGUP, KC_PGDN,  KC_END, XXXXXXX,    XXXXXXX, C(KC_WH_D), C(KC_WH_U), XXXXXXX, XXXXXXX,
   // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
-                XXXXXXX, _______, _______, XXXXXXX,    _______, XXXXXXX
-  //                   ╰───────────────────────────╯ ╰──────────────────╯
+               CTL_ENT, SFT_SPC, ALT_BSPC, XXXXXXX,   _______, XXXXXXX
+  //      ╰────────────────────────────────────────╯ ╰──────────────────────╯
   ),
 };
